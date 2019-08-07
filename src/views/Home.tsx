917b48318ec6f6f692b60ee39a3d1d4d1d49c4a2
Juliano Penna <julianopenna@gmail.com>
2019-08-07 12:42:05 -0700

    Setup initial dependencies

---
+++ src/views/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+export default function Home() {
+  return (
+    <div>
+      I am at home
+      <DatePicker />
+    </div>
+  )
+}
